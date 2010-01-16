08d3c36b5608ad6acc826b4ba4e75c2940544b62
Shabda Raaj <shabda@uswaretech.com>
2010-01-16 14:59:44 +0530

    Use localsettings

--- settings.py
+++ settings.py
@@ -9,12 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'carkitchen_ut_b'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'shabda'         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -80,3 +74,5 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     "wp",
 )
+
+from localsettings import *
