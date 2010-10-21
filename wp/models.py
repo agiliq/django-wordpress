f4a3ac06f0bb11949caa0e1341fbd64d21099c78
ashok.raavi <ashok.raavi@gmail.com>
2010-10-21 19:55:16 +0800

    changed models as per wordpress3.0 schema

--- wp/models.py
+++ wp/models.py
@@ -15,14 +15,14 @@ class WpUser(models.Model):
     user_activation_key = models.CharField(max_length=180)
     user_status = models.IntegerField()
     display_name = models.CharField(max_length=750)
+    
     class Meta:
-        verbose_name = "User"
-        verbose_name_plural = "Users"
+        verbose_name = u'User'
+        verbose_name_plural = u'Users'
         db_table = u'wp_users'
-        
+
     def __unicode__(self):
         return self.user_nicename
-
 
 
 class Link(models.Model):
@@ -31,10 +31,10 @@ class Link(models.Model):
     link_name = models.CharField(max_length=765)
     link_image = models.CharField(max_length=765)
     link_target = models.CharField(max_length=75)
-    link_category = models.IntegerField()
+    
     link_description = models.CharField(max_length=765)
     link_visible = models.CharField(max_length=60)
-    link_owner = models.ForeignKey(WpUser, db_column = "link_owner")
+    link_owner = models.ForeignKey(WpUser, db_column='link_owner')
     link_rating = models.IntegerField()
     link_updated = models.DateTimeField()
     link_rel = models.CharField(max_length=765)
@@ -42,12 +42,13 @@ class Link(models.Model):
     link_rss = models.CharField(max_length=765)
     
     class Meta:
-        verbose_name = "Link"
-        verbose_name_plural = "Link"
+        verbose_name = u'Link'
+        verbose_name_plural = u'Link'
         db_table = u'wp_links'
-        
+
     def __unicode__(self):
         return self.link_name
+
 
 class Option(models.Model):
     option_id = models.IntegerField(primary_key=True)
@@ -55,26 +56,28 @@ class Option(models.Model):
     option_name = models.CharField(unique=True, max_length=192)
     option_value = models.TextField()
     autoload = models.CharField(max_length=60)
+    
     class Meta:
-        verbose_name = "Option"
-        verbose_name_plural = "Options"
+        verbose_name = u'Option'
+        verbose_name_plural = u'Options'
         db_table = u'wp_options'
-        
+    
     def __unicode__(self):
         return self.option_name
+
 
 class Post(models.Model):
     id = models.IntegerField(db_column='ID', primary_key = True) # Field name made lowercase.
     guid = models.CharField(max_length=765)
-        
-    post_author = models.ForeignKey("wp.WpUser", db_column = "post_author")
-    post_parent = models.ForeignKey("self", db_column="post_parent")
+    
+    post_author = models.ForeignKey(WpUser, db_column='post_author')
+    post_parent = models.ForeignKey('self', db_column='post_parent')
     
     post_date = models.DateTimeField()
     post_date_gmt = models.DateTimeField()
     post_content = models.TextField()
     post_title = models.TextField()
-    post_category = models.IntegerField()
+    
     post_excerpt = models.TextField()
     post_status = models.CharField(max_length=60)
     post_name = models.CharField(max_length=600)
@@ -94,20 +97,18 @@ class Post(models.Model):
     
     menu_order = models.IntegerField()
     
-    
     class Meta:
-        verbose_name = "Post"
-        verbose_name_plural = "Posts"
+        verbose_name = u'Post'
+        verbose_name_plural = u'Posts'
         db_table = u'wp_posts'
-        
+   
     def __unicode__(self):
         return self.post_title or str(self.id)
     
     def get_absolute_url(self):
         return self.guid
-        #return "/%s/%s/%s/" % (self.post_date.year, self.post_date.month, (self.post_name or self.id))
-        
-    
+
+
 class PostMeta(models.Model):
     meta_id = models.IntegerField(primary_key=True)
     post = models.ForeignKey(Post)
@@ -116,8 +117,8 @@ class PostMeta(models.Model):
     meta_value = models.TextField(blank=True)
     
     class Meta:
-        verbose_name = "Post Meta"
-        verbose_name_plural = "Posts Meta"
+        verbose_name = u'Post Meta'
+        verbose_name_plural = u'Posts Meta'
         db_table = u'wp_postmeta'
     
     def __unicode__(self):
@@ -143,24 +144,26 @@ class Comment(models.Model):
     user_id = models.IntegerField()#We can not make this a FK as for WP sets the integer field to 0, in case of non registered users   
     
     class Meta:
-        verbose_name = "Comment"
-        verbose_name_plural = "Comments"
+        verbose_name = u'Comment'
+        verbose_name_plural = u'Comments'
         db_table = u'wp_comments'
-        
+
     def __unicode__(self):
         return self.comment_content[:50]
-        
+
+
 class CommentMeta(models.Model):
     meta_id = models.IntegerField(primary_key=True)
-    #comment = models.ForeignKey(Comment)
+    
     comment_id = models.IntegerField()
     meta_key = models.CharField(max_length=765, blank=True)
     meta_value = models.TextField(blank=True)
+
     class Meta:
-        verbose_name = "Comment Meta"
-        verbose_name_plural = "Comments Meta"
+        verbose_name = u'Comment Meta'
+        verbose_name_plural = u'Comments Meta'
         db_table = u'wp_commentmeta'
-        
+
     def __unicode__(self):
         return self.meta_key
 
@@ -172,10 +175,10 @@ class Term(models.Model):
     term_group = models.IntegerField()
     
     class Meta:
-        verbose_name = "Term"
-        verbose_name_plural = "Terms"
+        verbose_name = u'Term'
+        verbose_name_plural = u'Terms'
         db_table = u'wp_terms'
-        
+
     def __unicode__(self):
         return self.name
 
@@ -192,19 +195,20 @@ class TermTaxonomy(models.Model):
         verbose_name = "Term Taxonomy"
         verbose_name_plural = "Term Taxonomies"
         db_table = u'wp_term_taxonomy'
-    
+
     def __unicode__(self):
         return self.taxonomy
-    
+
+
 class TermRelationship(models.Model):
     object_id = models.IntegerField(primary_key=True)
     term_taxonomy = models.ForeignKey(TermTaxonomy)
     term_order = models.IntegerField()
+    
     class Meta:
-        verbose_name = "Term Relationship"
-        verbose_name_plural = "Term Relationships"
+        verbose_name = u'Term Relationship'
+        verbose_name_plural = u'Term Relationships'
         db_table = u'wp_term_relationships'
-        
 
 
 class UserMeta(models.Model):
@@ -213,34 +217,11 @@ class UserMeta(models.Model):
     
     meta_key = models.CharField(max_length=765, blank=True)
     meta_value = models.TextField(blank=True)
-    class Meta:
-        verbose_name = "User Meta"
-        verbose_name_plural = "Users Meta"
-        db_table = u'wp_usermeta'
     
+    class Meta:
+        verbose_name = 'User Meta'
+        verbose_name_plural = 'Users Meta'
+        db_table = u'wp_usermeta'
+
     def __unicode__(self):
         return self.user.user_nicename
-
-class YarppKeywordCache(models.Model):
-    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
-    body = models.TextField()
-    title = models.TextField()
-    date = models.DateTimeField()
-    class Meta:
-        verbose_name = "Yarpp Keyword Cache"
-        verbose_name_plural = "Yarpp Keyword Cache"
-        db_table = u'wp_yarpp_keyword_cache'
-        
-    def __unicode__(self):
-        return self.title or str(self.id)
-
-class YarppRelatedCache(models.Model):
-    reference_id = models.IntegerField(db_column='reference_ID') # Field name made lowercase.
-    id = models.IntegerField(primary_key=True, db_column='ID') # Field name made lowercase.
-    score = models.FloatField()
-    date = models.DateTimeField()
-    class Meta:
-        verbose_name = "Yarpp Related Cache"
-        verbose_name_plural = "Yarpp Related Cache"
-        db_table = u'wp_yarpp_related_cache'
-
