8e67234800d223f764f3c19b99c5d7274b239795
Bhaskar <bhaskar@agiliq.com>
2013-09-26 14:02:14 +0530

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "django-wordpress",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
     author = "Agiliq and friends",
     author_email ="shabda@agiliq.com", 
